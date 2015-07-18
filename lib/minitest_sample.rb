05fa7df0a34076f6e2ba7499520a1e66d884037b
okamuratakeshi <vagrant@enpit>
2015-07-18 08:54:31 +0000

    4kaime

--- lib/minitest_sample.rb
+++ lib/minitest_sample.rb
@@ -4,7 +4,7 @@ require "minitest_sample/version"
 module MinitestSample
   class Main
     def odd?(n)
-      if n == 1 || n == 3 || n == 5
+      if n == 1 || n == 3 || n == 5 || n == 7
         return true
       else
         return false
